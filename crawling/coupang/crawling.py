b3f512d1535a810c4aca01429602f181e48e41ad
choiseyeon <choi20014830@gmail.com>
2024-06-18 02:20:49 +0900

    refactor: 다음 페이지로 넘어가는 방식을 수정합니다.

--- crawling/coupang/crawling.py
+++ crawling/coupang/crawling.py
@@ -5,51 +5,47 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-
+from fake_useragent import UserAgent
 
 class CoupangCrawler:
-    def __init__(self, categories_id, max_pages=5):
+    def __init__(self, categories_id, max_pages=10):
         self.categories_id = categories_id
         self.max_pages = max_pages
         self.chrome_options = Options()
         self.chrome_options.add_argument("--no-sandbox")
         self.chrome_options.add_argument("--disable-dev-shm-usage")
-        self.chrome_options.add_argument(
-            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+        self.chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+        self.chrome_options.add_argument(f"user-agent={UserAgent().random}")
+        self.driver = None
 
     def start_driver(self):
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.chrome_options)
 
     def crawl_category(self, category_id):
-        for page in range(1, self.max_pages + 1):
-            self.start_driver()  # 페이지 로드마다 WebDriver 재시작
-            baseurl = (f'https://www.coupang.com/np/categories/{category_id}'
-                       f'?listSize=120&brand=&offerCondition=&filterType='
-                       f'&isPriceRange=false&minPrice=&maxPrice=&channel=user'
-                       f'&fromComponent=N&selectedPlpKeepFilter=&sorter=bestAsc&filter=&component=194186&rating=0'
-                       f'&page={page}')
+        self.start_driver()
+        baseurl = (f'https://www.coupang.com/np/categories/{category_id}'
+                   f'?listSize=120&brand=&offerCondition=&filterType='
+                   f'&isPriceRange=false&minPrice=&maxPrice=&channel=user'
+                   f'&fromComponent=N&selectedPlpKeepFilter=&sorter=bestAsc&filter=&component=194186&rating=0')
 
-            self.driver.get(baseurl)
+        self.driver.get(baseurl)
+        page = 1
 
+        while page <= self.max_pages:
             try:
-                # 특정 요소가 로드될 때까지 대기
                 WebDriverWait(self.driver, 20).until(
                     EC.presence_of_element_located((By.CLASS_NAME, 'baby-product-link'))
                 )
             except Exception as e:
                 print(f"페이지 로드 오류: 카테고리 {category_id}, 페이지 {page}")
                 print(f"Error: {e}")
-                self.driver.quit()
                 break
 
             items = self.driver.find_elements(By.CLASS_NAME, 'baby-product-link')
             if not items:
                 print(f"더 이상 항목이 없습니다: 카테고리 {category_id}, 페이지 {page}")
-                self.driver.quit()
                 break
 
-            cnt = 1
             for item in items:
                 try:
                     number = item.get_attribute('data-product-id')
@@ -58,30 +54,33 @@ class CoupangCrawler:
                     per_price = item.find_element(By.CLASS_NAME, 'unit-price').text.strip()
                     star = item.find_element(By.CLASS_NAME, 'rating').text.strip()
                     review_cnt = item.find_element(By.CLASS_NAME, 'rating-total-count').text.strip()
-
-                    print(f'카테고리 ID: {category_id}')
-                    print(f'페이지 상품 번호: {number}')
-                    print(f'페이지 상품 카운트: {cnt}')
-                    print(f'상품명: {title}')
-                    print(f'가격: {price}원')
-                    print(f'100g당 가격: {per_price}')
-                    print(f'별점: {star}')
-                    print(f'리뷰수: {review_cnt}')
-                    print('-' * 50)
-                    cnt += 1
-
+                    print(number, title, price, per_price, star, review_cnt)
                 except Exception as e:
-                    print(f"상품 정보 추출 오류: {e}")
+                    # print(f"상품 정보 추출 오류: {e}")
                     continue
 
-            self.driver.quit()
+            try:
+                next_page_button = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, f'//*[@id="product-list-paging"]/div/a[{page+2}]'))
+                )
+                self.driver.execute_script("arguments[0].click();", next_page_button)
+                WebDriverWait(self.driver, 20).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, 'baby-product-link'))
+                )
+                page += 1
+            except Exception as e:
+                print(f"다음 페이지로 이동할 수 없습니다: 카테고리 {category_id}, 페이지 {page}")
+                print(f"Error: {e}")
+                break
+        self.driver.quit()
 
     def crawl(self):
         for category_id in self.categories_id:
             self.crawl_category(category_id)
 
     def close(self):
-        self.driver.quit()
+        if self.driver:
+            self.driver.quit()
 
 
 if __name__ == '__main__':
