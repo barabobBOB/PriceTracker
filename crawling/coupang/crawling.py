28ec48ee5e40cb0bd5d5dc7456c7fb5b7f0202df
choiseyeon <choi20014830@gmail.com>
2024-06-19 22:48:23 +0900

    feat: BeautifulSoup으로 변경합니다.

--- crawling/coupang/crawling.py
+++ crawling/coupang/crawling.py
@@ -1,21 +1,20 @@
 import re
+import time
+import random
+import requests
 
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 
-from crawling.config.config import setup_logging, set_chrome_options
+from crawling.config.config import *
 from crawling.database.manager import DatabaseManager, db_config
 
 
-def construct_url(category_id):
+def construct_url(category_id, page):
     return (f'https://www.coupang.com/np/categories/{category_id}'
             f'?listSize=120&brand=&offerCondition=&filterType='
             f'&isPriceRange=false&minPrice=&maxPrice=&channel=user'
-            f'&fromComponent=N&selectedPlpKeepFilter=&sorter=bestAsc&filter=&component=194186&rating=0')
+            f'&fromComponent=N&selectedPlpKeepFilter=&sorter=bestAsc&filter=&component=194186&rating=0&page={page}')
 
 
 def setup_categories_id():
@@ -31,22 +30,21 @@ def setup_categories_id():
         194744, 194745, 194746, 194812
     ]
 
-
 class CoupangCrawler:
     def __init__(self, max_pages=10):
         self.categories_id = setup_categories_id()
-        self.driver = None
         self.logger = setup_logging()
         self.max_pages = max_pages
-        self.chrome_options = set_chrome_options()
         self.database_manager = DatabaseManager(db_config=db_config)
 
-    def start_driver(self):
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.chrome_options)
-
-    def close_driver(self):
-        if self.driver:
-            self.driver.quit()
+    def check_last_page(self, category_id):
+        response = requests.get(construct_url(category_id, 1), headers=set_header())
+        response.raise_for_status()
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            page = soup.find('div', class_='product-list-paging')
+            return int(page['data-total'])
+        return 1
 
     def crawl(self):
         try:
@@ -57,59 +55,35 @@ class CoupangCrawler:
             self.database_manager.close()
 
     def crawl_category(self, category_id):
-        self.start_driver()
-        base_url = construct_url(category_id)
-        self.driver.get(base_url)
-        page = 1
+        last_page = self.check_last_page(category_id)
+        time.sleep(random.randint(1, 3))
+        for page in range(1, min(last_page, self.max_pages) + 1):
+            self.crawl_page(category_id, page)
+            logging.info(f"------category_id: {category_id}, page: {page}------")
+            time.sleep(random.randint(1, 3))
 
-        while page <= self.max_pages:
-            if not self.load_page(category_id, page):
-                break
-
-            items = self.get_items()
-
-            if not items:
-                self.logger.info(f"더 이상 항목이 없습니다: 카테고리 {category_id}, 페이지 {page}")
-                break
-
-            self.extract_items(items, category_id)
-
-            if not self.go_to_next_page(page):
-                self.logger.info(f"다음 페이지로 이동할 수 없습니다: 카테고리 {category_id}, 페이지 {page}")
-                break
-
-            page += 1
-
-        self.close_driver()
-
-    def load_page(self, category_id, page):
-        try:
-            WebDriverWait(self.driver, 20).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'baby-product-link'))
-            )
-            return True
-        except Exception as e:
-            self.logger.error(f"페이지 로드 오류: 카테고리 {category_id}, 페이지 {page}")
-            self.logger.error(f"Error: {e}")
-            return False
-
-    def get_items(self):
-        return self.driver.find_elements(By.CLASS_NAME, 'baby-product-link')
+    def crawl_page(self, category_id, page):
+        url = construct_url(category_id, page)
+        response = requests.get(url, headers=set_header())
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'html.parser')
+        items = soup.find('ul', id='productList').find_all('li')
+        self.extract_items(items, category_id)
 
     def extract_items(self, items, category_id):
         for item in items:
             try:
-                product_id = item.get_attribute('data-product-id')
-                title = item.find_element(By.CLASS_NAME, 'name').text.strip()
-                price = item.find_element(By.CLASS_NAME, 'price-value').text.strip()
-                star = item.find_element(By.CLASS_NAME, 'rating').text.strip()
+                product_id = item['data-product-id']
+                title = item.find('div', class_='name').text
+                price = item.find('strong', class_='price-value').text
+                star = item.find('em', class_='rating').text
 
                 # per_price 괄호와 "100g당", "원" 제거
-                per_price = item.find_element(By.CLASS_NAME, 'unit-price').text.strip()
+                per_price = item.find('span', class_='unit-price').text
                 per_price = re.sub(r'\(100g당\s*|\s*원\)|\(|\)', '', per_price).strip()
 
                 # review_count 괄호 제거
-                review_count = item.find_element(By.CLASS_NAME, 'rating-total-count').text.strip()
+                review_count = item.find('span', class_='rating-total-count').text
                 review_count = re.sub(r'[\(\)]', '', review_count).strip()
 
                 # price와 per_price에서 쉼표 제거
@@ -132,23 +106,7 @@ class CoupangCrawler:
                 # 리뷰, 별점 등의 정보가 없는 경우
                 continue
 
-    def go_to_next_page(self, page):
-        try:
-            next_page_button = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f'//*[@id="product-list-paging"]/div/a[{page + 2}]'))
-            )
-            self.driver.execute_script("arguments[0].click();", next_page_button)
-            WebDriverWait(self.driver, 20).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'baby-product-link'))
-            )
-            return True
-        except Exception as e:
-            self.logger.error(f"다음 페이지로 이동할 수 없습니다: 페이지 {page}")
-            self.logger.error(f"Error: {e}")
-            return False
-
 
 if __name__ == '__main__':
     crawler = CoupangCrawler()
     crawler.crawl()
-    crawler.close_driver()
