44b1d8f8841ef2dcd16c53722ca92f2198c160dc
choiseyeon <choi20014830@gmail.com>
2024-07-12 21:53:21 +0900

    feat: 칼럼 타입을 바꿉니다.

--- dags/crawling/coupang/crawling.py
+++ dags/crawling/coupang/crawling.py
@@ -1,10 +1,14 @@
+import json
 import bs4
 import requests
 import datetime
+import logging
+import re
 
 from bs4 import BeautifulSoup
 from ..config.set_up import set_header, setup_logging, crawling_waiting_time
 from ..database.handler import DatabaseHandler
+from kafka import KafkaProducer
 
 
 def construct_url(category_id: int, page: int) -> str:
@@ -23,12 +27,12 @@ def check_last_page(category_id: int) -> int:
     return int(page['data-total'])
 
 
-def get_last_pages(categories_id: int, idx: int, **context) -> None:
+def get_last_pages(categories_id: list, idx: str, **context) -> None:
     last_pages = [check_last_page(category_id) for category_id in categories_id]
     context["task_instance"].xcom_push(key="last_pages_" + idx, value=last_pages)
 
 
-def create_url_list(categories_id: list[int], idx: int, **context) -> None:
+def create_url_list(categories_id: list[int], idx: str, **context) -> None:
     last_pages = context["task_instance"].xcom_pull(
         key="last_pages_" + idx
     )
@@ -40,13 +44,37 @@ def create_url_list(categories_id: list[int], idx: int, **context) -> None:
     context["task_instance"].xcom_push(key="url_list_" + idx, value=url_list)
 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def produce_to_kafka(data: dict) -> None:
+    logger.info("Kafka producer를 생성합니다.")
+    producer = KafkaProducer(
+        bootstrap_servers='kafka:29092',
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+    try:
+        if data:
+            logger.info("데이터를 Kafka 토픽으로 전송합니다: %s", data)
+            future = producer.send('coupang_crawling_topic', value=data)
+            logger.info("Kafka로 데이터 전송을 플러시합니다.")
+            producer.flush()
+            logger.info("데이터 전송이 성공적으로 완료되었습니다.")
+    except Exception as e:
+        logger.error("Kafka로 데이터 전송 중 오류 발생: %s", e)
+    finally:
+        logger.info("Kafka producer를 닫습니다.")
+        producer.close()
+
+
 class CoupangCrawler:
     def __init__(self) -> None:
         self.logger = setup_logging()
         self.db_handler = DatabaseHandler()
         self.db_handler.create_coupang_products()
 
-    def crawl(self, idx: int, **context) -> None:
+    def crawl(self, idx: str, **context) -> None:
         url_list = context["task_instance"].xcom_pull(
             key="url_list_" + idx
         )
@@ -55,7 +83,7 @@ class CoupangCrawler:
             self.crawl_page(url[0], url[1], idx, collection_datetime, **context)
             crawling_waiting_time()
 
-    def crawl_page(self, url: str, category_id: int, idx: int, collection_datetime: datetime, **context) -> None:
+    def crawl_page(self, url: str, category_id: int, idx: str, collection_datetime: datetime, **context) -> None:
         response = requests.get(url, headers=set_header())
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -79,13 +107,23 @@ class CoupangCrawler:
             try:
                 product["product_id"] = item['data-product-id']
                 product["title"] = item.find('div', class_='name').text
-                product["price"] = item.find('strong', class_='price-value').text
+
+                price = item.find('strong', class_='price-value').text
+                product["price"] = int(price.replace(',', ''))
                 product["star"] = item.find('em', class_='rating').text
-                product["per_price"] = item.find('span', class_='unit-price').text
-                product["review_count"] = item.find('span', class_='rating-total-count').text
+
+                per_price = item.find('span', class_='unit-price').text
+                per_price = re.sub(r'\(100g당\s*|\s*원\)|\(|\)', '', per_price).strip()
+                product["per_price"] = int(per_price.replace(',', ''))
+
+                review_count = item.find('span', class_='rating-total-count').text
+                product["review_count"] = re.sub(r'[\(\)]', '', review_count).strip()
+
                 product["category_id"] = category_id
                 product["collection_datetime"] = collection_datetime
+
                 self.db_handler.insert_product(product)
+                produce_to_kafka(product)
 
             except Exception:
                 # 리뷰, 별점 등의 정보가 없는 경우
