9c683fbc94006906d36cb2ad46cdb0ac0cc4236b
최세연 (Lena) <choi20014830@gmail.com>
2024-12-30 17:30:48 +0900

    Revert "refactor: 카프카 프로듀싱 로직을 제거합니다."
    This reverts commit 747f563f60a58946feebf3163502a4234f8d1836.

--- dags/crawling/coupang/crawling.py
+++ dags/crawling/coupang/crawling.py
@@ -5,6 +5,7 @@ import re
 
 from bs4 import BeautifulSoup
 
+from ..config.kafka_produce import produce_to_kafka
 from ..config.set_up import set_header, setup_logging, crawling_waiting_time
 from ..database.handler import DatabaseHandler
 
@@ -97,6 +98,7 @@ class CoupangCrawler:
                 self.db_handler.insert_product(product)
 
                 product["collection_datetime"] = str(collection_datetime)
+                produce_to_kafka(product)
 
             except Exception:
                 # 리뷰, 별점 등의 정보가 없는 경우
